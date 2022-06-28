c08e0bd63b43c148e746da35543cc771d071378b
jaehyeongAN <nonamed000000@gmail.com>
2022-06-28 17:33:20 +0900

    ud

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
 # RedisAI-demo
+
+## Setup
+### serving-by-fastapi
+1. Git clone 
+```bash
+git clone https://github.com/jaehyeongAN/RedisAI-demo.git
+```
+
+2. Move to dir
+```bash
+cd RedisAI-demo/serving-by-fastapi
+```
+
+3. Install Dependencies
+```bash
+pip install -r requirements.txt
+```
+
+4. Run FastAPI server
+```bash
+uvicorn main:app 
+```
+
+### redis-cluster-by-docker-compose
+1. Git clone 
+```bash
+git clone https://github.com/jaehyeongAN/RedisAI-demo.git
+```
+
+2. Move to dir
+```bash
+cd RedisAI-demo/redis-cluster-by-docker-compose
+```
+
+3. Install Dependencies
+```bash
+pip install -r requirements.txt
+```
+
+4. ...
+
+
+
+## Endpoint 
+### serving-by-fastapi
+1. <code>/train</code>
+ - 1. Load iris dataset.
+ - 2. Train model using LogisticRegression of sklearn.
+ - 3. Conver sklearn model to ONNX model.
+ - 4. Save ONNX model to RedisAI
+```bash
+http GET localhost:8000/train
+```
+
+2. <code>/inference</code>
+ - 1. Check Model 
+ - 2. Set input tensor to RedisAI
+ - 3. Model execute
+ - 4. Get result
+```bash
+http GET "localhost:8000/inference?model=iris-clf&sepal_length=4.7&sepal_width=3.2&petal_length=1.6&petal_width=0.2"
+```
+![](./tmp/inference-fastapi.png)
+
+
+### redis-cluster-by-docker-compose
+ - 1. ...
--- serving-by-fastapi/main.py
+++ serving-by-fastapi/main.py
@@ -29,31 +29,29 @@ MODEL_OUTPUT_PATH = './_output/iris.onnx'
 
 @app.get('/train')
 async def train():
-	# Prepare the train and test data
+	# 1. Prepare the train and test data
 	iris = load_iris()
 	X, y = iris.data, iris.target
 	X_train, X_test, y_train, y_test = train_test_split(X, y)
 
-	# Train the model - using logistic regression classifier
+	# 2. Train the model - using logistic regression classifier
 	model = LogisticRegression(max_iter=5000)
 	model.fit(X_train, y_train)
 
-
-	# Convert sklearn model to ONNX model
+	# 3. Convert sklearn model to ONNX model
 	dummy = X_train[0].astype(np.float32)
 	save_sklearn(model, MODEL_OUTPUT_PATH, prototype=dummy) # save model to MODEL_OUTPUT_PATH
-
 	
-	# Set ONNX model to RedisAI
+	# 4. Set ONNX model to RedisAI
 	model_name = 'iris-clf'
 	model = load_model(MODEL_OUTPUT_PATH)
 	redisai_client.modelset('iris-clf', 'onnx', 'cpu', model)
-	print(f'>> {model_name} model is saved to RedisAI 🟢.')
+	print(f">> '{model_name}'' model is saved to RedisAI 🟢.")
 
 
 @app.get('/inference') 
 async def inference(model: str, sepal_length: float, sepal_width: float, petal_length: float, petal_width: float):
-	# Check Model in RedisAI
+	# 1. Check Model in RedisAI
 	try:
 		model_meta = redisai_client.modelget(f'{model}', meta_only=True)
 		print(f">> '{model}'' model is active on RedisAI 🟢.")
@@ -63,19 +61,20 @@ async def inference(model: str, sepal_length: float, sepal_width: float, petal_l
 		print(f">>{model}' model is not active on RedisAI. 🔴")
 		return JSONResponse({'status':'fail', 'class':None})
 
-	# Set input tensor
-	input_value = [sepal_length, sepal_width, petal_length, petal_width]
-	intput_tensor = np.array(input_value).astype(np.float32)
-	redisai_client.tensorset(f'{model}:in', intput_tensor)
+	if model_meta:
+		# 2. Set input tensor
+		input_value = [sepal_length, sepal_width, petal_length, petal_width]
+		intput_tensor = np.array(input_value).astype(np.float32)
+		redisai_client.tensorset(f'{model}:in', intput_tensor)
 
-	# Predict 
-	redisai_client.modelexecute(model, inputs=[f'{model}:in'], outputs=[f'{model}:out1', f'{model}:out2'])
+		# 3. Predict
+		redisai_client.modelexecute(model, inputs=[f'{model}:in'], outputs=[f'{model}:out1', f'{model}:out2'])
 
-	# Get result
-	out = redisai_client.tensorget(f'{model}:out1')[0]
-	labels = ['setosa', 'versicolor', 'virginica']
-	print(labels[out])
+		# 4. Get result
+		out = redisai_client.tensorget(f'{model}:out1')[0]
+		labels = ['setosa', 'versicolor', 'virginica']
+		print(labels[out])
 
-	return JSONResponse({'status':'success', 'pred':labels[out]})
+		return JSONResponse({'status':'success', 'pred':labels[out]})
 
 
